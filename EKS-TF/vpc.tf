a024170edf5759a2b99884df5375ee2d2c9f4a8e
Tural Aghazada <141626180+taghazada@users.noreply.github.com>
2024-06-12 14:59:02 +0400

    Update vpc.tf
    changed vpc ip

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -28,7 +28,7 @@ data "aws_security_group" "sg-default" {
 
 resource "aws_subnet" "public-subnet2" {
   vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.1.0.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 
