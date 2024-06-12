c00ea1edc9aecf89e8797102f01413706dde33cd
Tural Aghazada <141626180+taghazada@users.noreply.github.com>
2024-06-12 14:30:41 +0400

    Update vpc.tf

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -28,7 +28,7 @@ data "aws_security_group" "sg-default" {
 
 resource "aws_subnet" "public-subnet2" {
   vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 
